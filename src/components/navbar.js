04d19c8f5fdabcc47a156baabac55f079a7a1ed3
“noxar94” <Lundmark1223@hotmail.se>
2023-05-16 11:32:01 +0200

    making navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
+import "../styles/navbar.css";
 
 export default function navbar() {
+  const navRef = useRef();
+  const showNavbar = () => {};
   return (
     <header>
-      <nav>
+      <nav ref={navRef}>
+        <h2>My Logo</h2>
         <Link to="/"></Link>
         <Link to="/game"></Link>
+
+        <button onClick={showNavbar}></button>
       </nav>
+      <button onClick={showNavbar}></button>
     </header>
   );
 }
